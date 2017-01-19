eeff95bc7341eb066fca0edc3dd8b72e2fc77e39
saurabhdash <saurabhdash2512@gmail.com>
2017-01-19 15:50:13 +0530

    partC

--- Exp1/DFTsqr.m
+++ Exp1/DFTsqr.m
@@ -1,13 +1,14 @@
-function DFT(L,Fs)                                                   %L = Length of signal
+function y = DFTsqr(L,Fs)                                                  %L = Length of signal
 Fs = Fs * 1e3;                                                             %Sampling frequency
 T = 1/Fs;                                                                  %Sampling period
 t = (0:L-1)*T;                                                             % Time vector
-x = 10*cos(2*pi*1e3*t)+6*cos(2*pi*2*1e3*t)+2*cos(2*pi*4*1e3*t);            % Signal Generated  
+x = square(2*pi*1e3*t);                                                    % Signal Generated  
 f = Fs *(0:L-1)/L;                                                         % frequency array                                                
 y = circshift(abs(fft(x,L))/L,[0 L/2]);                                    % DFT of the Signal using the FFT algorithm                                      
 figure;                                                                    % New figure for plotting
 plot(f- Fs/2 ,y);                                                          % DFT Plotted                                          
 xlabel('Frequency');                                                       % X label
 ylabel('|X(f)|/N');                                                        % Y label
-title(['DFT of signal with N = ' num2str(L) ', Fs = ' num2str(Fs/1000) 'kHz']); %Title
+title(['DFT of  Square signal with N = ' num2str(L) ', Fs = ' num2str(Fs/1000) 'kHz']); %Title
+y = fft(x,L);
 end
--- Exp1/script1.m
+++ Exp1/script1.m
@@ -1,3 +1,6 @@
+clear;
+clc;
+%{
 %% Creating the Original Signal and Sampling at 12kHz
 Fs = 12e3;                                                                 %Setting the Sampling Rate
 t=0:0.01*10^-3:(100-0.01)*10^-3;                                           %Creating the time array for original signal
@@ -10,6 +13,7 @@ stem(t1,x1);                                                               %Plot
 xlabel('Time');                                                            %X-Axis Label
 ylabel('x(t)');                                                            %Y-Axis Label
 title('original and sampled signal');                                      %Plot title
+xlim([0.039, 0.0415]);
 %% DFT of the Sampled Signal at 12kHz
 DFT(10000,12);                                                             %Number of samples = 1e4 , Sampling Frequency = 12kHz
 DFT(64,12);                                                                %Number of samples = 64 , Sampling Frequency = 12kHz
@@ -19,3 +23,19 @@ DFT(256,12);                                                               %Numb
 DFT(10000,8);                                                              %Sampling Frequency = 8kHz, Number of samples = 1e4
 DFT(10000,5);                                                              %Sampling Frequency = 5kHz, Number of samples = 1e4
 DFT(10000,4);                                                              %Sampling Frequency = 4kHz, Number of samples = 1e4
+%% Generating and Sampling a Square Wave
+%}
+t= 0:1/(40e4):3e-3;
+x = square(2*pi*1e3*t);
+ylim([-2,2]);
+t1 = 0:1/20e3:3e-3;
+x1 = square(2*pi*1e3*t1);
+plot(t,x);
+hold on;
+stem(t1,x1); 
+%% DFT of Square Wave
+y = DFTsqr(256,20);
+%% Reconstruction of the Signal from the DFT
+figure;
+x = ifft(y);
+plot(x)
