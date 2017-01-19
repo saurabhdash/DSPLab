4def8ab9d124d8c5a03310c929ccfaa30f974a8c
saurabhdash <saurabhdash2512@gmail.com>
2017-01-19 14:50:27 +0530

    Commenting

--- Exp1/script1.m
+++ Exp1/script1.m
@@ -1,28 +1,21 @@
-Fs = 12e3;
-t=0:0.01*10^-3:(100-0.01)*10^-3;
-t1 = 0:1/Fs:(100-0.01)*10^-3;
-x=10*cos(2*pi*1e3*t)+6*cos(2*pi*2*1e3*t)+2*cos(2*pi*4*1e3*t);
-x1=10*cos(2*pi*1e3*t1)+6*cos(2*pi*2*1e3*t1)+2*cos(2*pi*4*1e3*t1);
-plot(t,x);
+%% Creating the Original Signal and Sampling at 12kHz
+Fs = 12e3;                                                                 %Setting the Sampling Rate
+t=0:0.01*10^-3:(100-0.01)*10^-3;                                           %Creating the time array for original signal
+t1 = 0:1/Fs:(100-0.01)*10^-3;                                              %Creating the time 
+x=10*cos(2*pi*1e3*t)+6*cos(2*pi*2*1e3*t)+2*cos(2*pi*4*1e3*t);              % Original Function
+x1=10*cos(2*pi*1e3*t1)+6*cos(2*pi*2*1e3*t1)+2*cos(2*pi*4*1e3*t1);          % Sampled function
+plot(t,x);                                                                 %Plotting the original function
 hold on;
-stem(t1,x1);
-xlabel('Time');
-ylabel('x(t)');
-title('original and sampled signal');
-%{
-figure;
-y= circshift(abs(fft(x))/10000,[0 5000]);
-plot([-50000:10:49999], y);
-xlabel('Frequency');
-ylabel('|X(f)|');
-ylabel('|X(f)|/N');
-title('DFT of original signal');
-%}
-
-DFT(10000,12);
-DFT(64,12);
-DFT(128,12);
-DFT(256,12);
-
-DFT(10000,8);
-
+stem(t1,x1);                                                               %Plotting the sampled function
+xlabel('Time');                                                            %X-Axis Label
+ylabel('x(t)');                                                            %Y-Axis Label
+title('original and sampled signal');                                      %Plot title
+%% DFT of the Sampled Signal at 12kHz
+DFT(10000,12);                                                             %Number of samples = 1e4 , Sampling Frequency = 12kHz
+DFT(64,12);                                                                %Number of samples = 64 , Sampling Frequency = 12kHz
+DFT(128,12);                                                               %Number of samples = 128 , Sampling Frequency = 12kHz
+DFT(256,12);                                                               %Number of samples = 256 , Sampling Frequency = 12kHz
+%% DFT of the Sampled Signal at frequencies lower than Nyquist Frequencies
+DFT(10000,8);                                                              %Sampling Frequency = 8kHz, Number of samples = 1e4
+DFT(10000,5);                                                              %Sampling Frequency = 5kHz, Number of samples = 1e4
+DFT(10000,4);                                                              %Sampling Frequency = 4kHz, Number of samples = 1e4
