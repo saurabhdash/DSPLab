115e5d2afa13cdd5a1aeb4772f117fa11532bd52
Aditya Sinha <adityasinha379@gmail.com>
2017-02-23 13:00:19 +0530

    windowed FIR lpf made

--- Exp2/Experiment2.m
+++ Exp2/Experiment2.m
@@ -1,7 +1,7 @@
 clear;
 clc;
 %%
-N = 32;
+N = 513;
 wc = 0.3*pi;
 %%
 hd = LPFilt(wc,N);
@@ -10,37 +10,38 @@ hd = LPFilt(wc,N);
 % ax.XLim = [1 N];
 %%
 w = rectWindow(N);
-B = hd .* w;
+B1 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
-title('Rectanguar Window');
+freqz(B1,1,-0.9*pi:0.005:pi);
+title(['Rectanguar Window (N=' num2str(N) ')']);
 %%
 w = triWindow(N);
-B = hd .* w;
+B2 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
-title('Trianguar Window');
+freqz(B2,1,-pi:0.005:pi);
+title(['Trianguar Window (N=' num2str(N) ')']);
 %%
 w = hannWindow(N);
-B = hd .* w;
+B3 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
-title('Hanning Window');
+freqz(B3,1,-pi:0.1:pi);
+title(['Hanning Window (N=' num2str(N) ')']);
 %%
 w = hammWindow(N);
-B = hd .* w;
+B4 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
-title('Hamming Window');
+freqz(B4,1,-pi:0.005:pi);
+title(['Hamming Window (N=' num2str(N) ')']);
 %%
 w = blackWindow(N);
-B = hd .* w;
+B5 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
-title('Backmann Window');
+freqz(B5,1,-pi:0.005:pi);
+title(['Blackman Window (N=' num2str(N) ')']);
 %%
+%{
 Fs = Fs * 1e3;            % Sampling frequency
 T = 1/Fs;             % Sampling period
 L = 10000;             % Length of signal
 t = (0:L-1)*T;        % Time vector
-%%
+%}
