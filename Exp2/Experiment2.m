5893f089b20f960620ae99156b921344f44478bd
saurabhdash <saurabhdash2512@gmail.com>
2017-02-23 13:03:01 +0530

    Part(b)

--- Exp2/Experiment2.m
+++ Exp2/Experiment2.m
@@ -5,42 +5,45 @@ N = 32;
 wc = 0.3*pi;
 %%
 hd = LPFilt(wc,N);
-% plot(hd);
-% ax = gca;
-% ax.XLim = [1 N];
+%plot(hd);
 %%
 w = rectWindow(N);
-B = hd .* w;
+B1 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
+freqz(B,1,-0.9*pi:0.01:pi);
 title('Rectanguar Window');
 %%
 w = triWindow(N);
-B = hd .* w;
+B2 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
+freqz(B,1,-0.9*pi:0.01:pi);
 title('Trianguar Window');
 %%
 w = hannWindow(N);
-B = hd .* w;
+B3 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
+freqz(B,1,-0.9*pi:0.01:pi);
 title('Hanning Window');
 %%
 w = hammWindow(N);
-B = hd .* w;
+B4 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
+freqz(B,1,-0.9*pi:0.01:pi);
 title('Hamming Window');
 %%
 w = blackWindow(N);
-B = hd .* w;
+B5 = hd .* w;
 figure;
-freqz(B,1,-pi:0.1:pi);
+freqz(B,1,-0.9*pi:0.01:pi);
 title('Backmann Window');
 %%
-Fs = Fs * 1e3;            % Sampling frequency
+Fs = 10 * 1e3;            % Sampling frequency
 T = 1/Fs;             % Sampling period
-L = 10000;             % Length of signal
+L = 500;             % Length of signal
 t = (0:L-1)*T;        % Time vector
+x = sin(2*pi*0.1*Fs*t)+sin(2*pi*0.8*Fs*t);
+%plot(t,x);
+plotdft(x,Fs,'input FFT')
 %%
+y = filtfilt(B,1,x);
+plotdft(y,Fs,'output FFT')
