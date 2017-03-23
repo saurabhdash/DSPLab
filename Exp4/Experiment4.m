cc0c13ed453aca441bbf2de79eec2aed8528c505
Aditya Sinha <adityasinha379@gmail.com>
2017-03-23 14:00:26 +0530

    Part 2 done. Truncation of coefficient bits left.

--- Exp4/Experiment4.m
+++ Exp4/Experiment4.m
@@ -11,47 +11,45 @@ Upper passband edge: 0.3pi          Upper stopband edge: 0.31pi
 6. IIR Inverse Chebyshev
 7. IIR Elliptic
 %}
-%% 1.b.FIR-Hannwindow
-%{
+%% 1.a FIR-Rectangular window
+N=512;
+h1 = fir1(N, [0.199 0.301], 'bandpass', rectwin(N+1));
+freqz(h1, 1, 10000);
+%% 1.b.FIR-Hanning window
 N=400;
 h1 = fir1(N, [0.198 0.302], 'bandpass', hann(N+1));
+figure;
 freqz(h1,1,10000);
-%}
 %% 2.FIR-LS
-%{
 h1 = firls(155,[0 0.19 0.203 0.297 0.31 1],[0 0 1 1 0 0]);
+figure;
 freqz(h1,1,10000);
-%}
 %% 3.FIR-Parks-McClellan
-%{
 N=175;
 h1=firpm(N,[0 0.19 0.205 0.295 0.31 1],[0 0 1 1 0 0]);
+figure;
 freqz(h1,1,10000);
-%}
 %% 4.Butterworth IIR
-%{
 [z,p,k] = butter(22,[0.2 0.3],'bandpass');
 sos = zp2sos(z,p,k);
 fvtool(sos);
 xlim([0.18 0.32]);
 ylim([-45 5]);
-%}
 %% 5.Chebyshev IIR
-%[b,a] = cheby1(9,1,[0.2 0.3],'bandpass');
-%freqz(b,a);
+[z,p,k] = cheby1(9,1,[0.2 0.3],'bandpass');
+sos = zp2sos(z,p,k);
+fvtool(sos);
+xlim([0.18 0.32]);
+ylim([-45 5]);
 %% 6.Inverse Chebyshev IIR
-%{
 [z,p,k] = cheby2(7,30,[0.19 0.31],'bandpass');
 sos = zp2sos(z,p,k);
 fvtool(sos);
 xlim([0.18 0.32]);
 ylim([-45 5]);
-%}
 %% 7.Elliptic IIR
-%{
 [z,p,k] = ellip(5,1,30,[0.2 0.3],'bandpass');
 sos = zp2sos(z,p,k);
 fvtool(sos);
 xlim([0.18 0.32]);
 ylim([-45 5]);
-%}
