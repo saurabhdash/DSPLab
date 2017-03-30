d16c3b06460727eb72f5ad329da99c9325c4e323
Aditya Sinha <adityasinha379@gmail.com>
2017-03-30 13:22:31 +0530

    Bit precision done for Butter and Cheby

--- Exp4/Experiment4.m
+++ Exp4/Experiment4.m
@@ -14,11 +14,10 @@ Upper passband edge: 0.3pi          Upper stopband edge: 0.31pi
 %% 1.a FIR-Rectangular window
 N=512;
 h1 = fir1(N, [0.199 0.301], 'bandpass', rectwin(N+1));
-%freqz(h1, 1, 10000);
+freqz(h1, 1, 10000);
 %figure;
-h1_10 = bitprecision(10,1,N+1,h1);     %used for finite word length
-%freqz(h1_dash,1,10000);
-fvtool(h1,1,h1_10,1);
+%h1_dash = bitprecision(10,1,N+1,h1);     %used for finite word length
+%fvtool(h1,1,h1_dash,1);
 %% 1.b.FIR-Hanning window
 N=400;
 h1 = fir1(N, [0.198 0.302], 'bandpass', hann(N+1));
@@ -34,13 +33,13 @@ h1=firpm(N,[0 0.19 0.205 0.295 0.31 1],[0 0 1 1 0 0]);
 figure;
 freqz(h1,1,10000);
 %% 4.Butterworth IIR
-[z,p,k] = butter(22,[0.2 0.3],'bandpass');
+[z,p,k] = butter(52,[0.2 0.3],'bandpass');
 sos = zp2sos(z,p,k);
 fvtool(sos);
 xlim([0.18 0.32]);
 ylim([-45 5]);
 %% 5.Chebyshev IIR
-[z,p,k] = cheby1(9,1,[0.2 0.3],'bandpass');
+[z,p,k] = cheby1(18,1,[0.2 0.3],'bandpass');
 sos = zp2sos(z,p,k);
 fvtool(sos);
 xlim([0.18 0.32]);
